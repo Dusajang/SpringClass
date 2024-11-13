1f9a7e1006cfdc9e1eeab92315fe5d39fdb32720
Dusajang <tlswjseks@gmail.com>
2024-11-13 14:32:50 +0900

    Spring day 01

--- SL00_DefaultSettings/src/main/java/org/doit/ik/mapper/TimeMapper.java
+++ SL00_DefaultSettings/src/main/java/org/doit/ik/mapper/TimeMapper.java
@@ -1,8 +1,13 @@
 package org.doit.ik.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface TimeMapper {
 
 	String getTime();
+	
+	@Select("SELECT SYSDATE+1 FROM dual")
+	String getNextTime();
 	
 	
 }
